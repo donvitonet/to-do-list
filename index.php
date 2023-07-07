632f7d547cc12001ecf5fbc258ea7662d80ff808
Reginaldo Lima <donvitonet@gmail.com>
2023-07-06 21:38:29 -0300

    feat: adiciona o router no container de depedências.

--- index.php
+++ index.php
@@ -19,6 +19,7 @@ include "src/app/controllers/update-task-controller.php";
 include "src/app/controllers/delete-task-controller.php";
 include "src/app/controllers/complete-task-controller.php";
 include "src/app/controllers/uncomplete-task-controller.php";
+include "src/app/controllers/cors-controller.php";
 include "src/app/controllers/not-found-controller.php";
 
 include "src/infra/tools/validator-schema.php";
@@ -37,17 +38,22 @@ $container = new DIContainer([
   UpdateTaskController::class,
   DeleteTaskController::class,
   CompleteTaskController::class,
-  UncompleteTaskController::class
+  UncompleteTaskController::class,
+  CORSController::class,
+  NotFoundController::class,
+  Router::class
 ]);
 
-$router = new Router();
-$router->addRoute(new Route('GET', '/', $container->get(HomeController::class)));
-$router->addRoute(new Route('GET', '/tasks', $container->get(TasksController::class)));
-$router->addRoute(new Route('GET', '/task', $container->get(DetailTaskController::class)));
-$router->addRoute(new Route('POST', '/task', $container->get(CreateTaskController::class)));
-$router->addRoute(new Route('PUT', '/task', $container->get(UpdateTaskController::class)));
-$router->addRoute(new Route('DELETE', '/task', $container->get(DeleteTaskController::class)));
-$router->addRoute(new Route('PATCH', '/task/complete', $container->get(CompleteTaskController::class)));
-$router->addRoute(new Route('PATCH', '/task/uncomplete', $container->get(UncompleteTaskController::class)));
+$router = $container->get(Router::class);
+$router->addRoute(new Route('GET', '/', HomeController::class));
+$router->addRoute(new Route('GET', '/tasks', TasksController::class));
+$router->addRoute(new Route('POST', '/tasks', CreateTaskController::class));
+$router->addRoute(new Route('OPTIONS', '/tasks', CORSController::class));
+
+$router->addRoute(new Route('GET', '/task', DetailTaskController::class));
+$router->addRoute(new Route('PUT', '/task', UpdateTaskController::class));
+$router->addRoute(new Route('DELETE', '/task', DeleteTaskController::class));
+$router->addRoute(new Route('PATCH', '/task/complete', CompleteTaskController::class));
+$router->addRoute(new Route('PATCH', '/task/uncomplete', UncompleteTaskController::class));
 
 $router->dispatch(new Request());
--- src/infra/http/router.php
+++ src/infra/http/router.php
@@ -2,37 +2,32 @@
 
 class Router
 {
+  private $container;
   private $routes = array();
+
+
+  public function __construct($container = null)
+  {
+    $this->container = $container;
+  }
 
   public function addRoute(Route $route)
   {
     $this->routes[$route->baseURL] = $route;
   }
 
-  private function getRoute(Request $request)
+  private function getHandler(Request $request)
   {
     if (array_key_exists($request->baseURL, $this->routes)) {
-      return $this->routes[$request->baseURL];
+      return $this->routes[$request->baseURL]->handler;
     }
 
-    return null;
+    return NotFoundController::class;
   }
 
   public function dispatch(Request $request)
   {
-    $route = $this->getRoute($request);
-    if ($route) {
-      try {
-        $route->handler->run($request);
-      } catch (\Throwable $th) {
-        error_log(print_r($th, true));
-      }
-
-
-      return;
-    }
-
-    $notFoundHandler = new NotFoundController();
-    $notFoundHandler->run($request);
+    $handler = $this->getHandler($request);
+    $this->container->get($handler)->run($request);
   }
 }
