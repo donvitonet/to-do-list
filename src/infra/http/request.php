7c05e88e7c9ed096ce313e38195cffd3aa8ed2b2
Reginaldo Lima <donvitonet@gmail.com>
2023-07-09 21:31:44 -0300

    feat: Adiciona path params no objeto da requisição.

--- src/infra/http/request.php
+++ src/infra/http/request.php
@@ -2,29 +2,62 @@
 
 class Request
 {
-  public $url;
-  public $method;
-  public $query;
-  public $payload;
+  public string $url;
+  public string $method;
+  public array $query;
+  public array $body;
+  public stdClass $params;
 
   public $baseURL;
 
   public function __construct()
   {
-    $this->url = $_SERVER['REQUEST_URI'];
     $this->method = $_SERVER['REQUEST_METHOD'];
-
-    $this->payload = json_decode(file_get_contents('php://input'), true);
-    if (empty($this->payload)) {
-      $this->payload = array();
-    }
-
-    if (parse_url($this->url, PHP_URL_QUERY)) {
-      parse_str(parse_url($this->url, PHP_URL_QUERY), $this->query);
-    } else {
-      $this->query = array();
-    }
-
+    $this->url = $_SERVER['REQUEST_URI'];
+    $this->query = self::parseQuery($this->url);
+    $this->body = self::parseBody();
     $this->baseURL = BaseURL::generate($this->url, $this->method);
+    $this->params = self::parseParams($this);
+  }
+
+  private static function parseParams(Request $request)
+  {
+    $params = new stdClass();
+
+    $basePaths = explode('_', $request->baseURL);
+    $param = array_search('PARAM', $basePaths);
+    if ($param === false) {
+      return $params;
+    }
+
+    $urlPaths = explode('/', $request->url);
+    $urlPaths = array_filter($urlPaths, function($value) {
+      return !empty($value);
+    });
+    $urlPaths = array_values($urlPaths);
+
+    $params->id = $urlPaths[$param - 2];
+    return $params;
+  }
+
+  private static function parseBody()
+  {
+    $body = json_decode(file_get_contents('php://input'), true);
+    if (empty($body)) {
+      return array();
+    }
+
+    return $body;
+  }
+
+  private static function parseQuery(string $url)
+  {
+    $query = array();
+
+    if (parse_url($url, PHP_URL_QUERY)) {
+      parse_str(parse_url($url, PHP_URL_QUERY), $query);
+    }
+
+    return $query;
   }
 }
