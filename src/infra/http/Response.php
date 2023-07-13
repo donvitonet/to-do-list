f76fdd09aca6f237b8260a4ab37890ecba3b973a
Reginaldo Lima <donvitonet@gmail.com>
2023-07-13 06:40:00 -0300

    feat: Adiciona Response::sendStatus com mensagens pre definidas.

--- src/infra/http/Response.php
+++ src/infra/http/Response.php
@@ -2,8 +2,15 @@
 
 namespace infra\http;
 
+use Exception;
+
 class Response
 {
+  public static function sendStatus($httpResponseCode)
+  {
+    self::send(array(), $httpResponseCode);
+  }
+
   public static function send(
     array $body = array(),
     int $httpResponseCode = 200
@@ -11,6 +18,33 @@ class Response
     header("Access-Control-Allow-Origin: *");
     header('Content-Type: application/json; charset=utf-8');
     http_response_code($httpResponseCode);
-    echo json_encode($body);
+
+    $noBody = empty($body);
+    $containDefaultMessage = in_array($httpResponseCode, array(400, 404, 500));
+
+    if ($noBody && $containDefaultMessage) {
+      $body = array(
+        'message' => self::getDefaultBodyByCode($httpResponseCode)
+      );
+    }
+
+    $hasBody = !empty($body);
+    if ($hasBody) {
+      echo json_encode($body);
+    }
+  }
+
+  private static function getDefaultBodyByCode($httpResponseCode)
+  {
+    switch ($httpResponseCode) {
+      case 400:
+        return "Requisição inválida";
+      case 404:
+        return "Não encontrado";
+      case 500:
+        return "Ocorreu um erro ao processar a requisição";
+      default:
+        throw new Exception("Code not found");
+    }
   }
 }
